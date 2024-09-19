a827763eacd62c8a854f16a1ab85aa8a4b016a39
GertNingbers <97081115@st.deltion.nl>
2024-09-19 14:08:20 +0200

    V1 Quiz

--- quiz.php
+++ quiz.php
@@ -1,3 +1,21 @@
 <?php
 
+echo "Welkom bij mijn quiz!\n";
+echo "\n";
+
+$random = rand(1,100);
+echo "$random\n";
+$invoer = readline ("gok een nummer tussen de 1 en 100: ");
+echo "\n";
+if ( $random == $invoer ) {
+    echo "Gefeliciteerd je hebt goed gegokt\n"; 
+}
+if ( $random < $invoer ) {
+    echo " Oef je moet wat lager gokken\n";
+}
+if ( $random > $invoer ) {
+    echo "Ah wat jammer je moet wat hooger gokken\n";
+}
+
+
 ?>
