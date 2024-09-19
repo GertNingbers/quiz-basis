fc6ab2f305744a615ea23920ac1a050e4870eb87
GertNingbers <97081115@st.deltion.nl>
2024-09-19 16:15:04 +0200

    V3 quiz

--- quiz.php
+++ quiz.php
@@ -3,19 +3,19 @@
 echo "Welkom bij mijn quiz!\n";
 echo "\n";
 
+$maximum=1;
 $random = rand(1,100);
 echo "$random\n";
 $invoer = readline ("gok een nummer tussen de 1 en 100: ");
 echo "\n";
 if ( $random == $invoer ) {
-    echo "Gefeliciteerd je hebt goed gegokt\n"; 
+    echo "Gefeliciteerd je hebt goed gegokt.\n"; 
 }
 if ( $random < $invoer ) {
-    echo " Oef je moet wat lager gokken\n";
+    echo " Oef je moet wat lager gokken.\n";
 }
 if ( $random > $invoer ) {
-    echo "Ah wat jammer je moet wat hooger gokken\n";
-}
-
+    echo "Ah wat jammer je moet wat hooger gokken.\n";
+} 
 
 ?>
---
+++ test.php
@@ -0,0 +1,36 @@
+<?php
+
+echo "Welkom bij mijn quiz!\n";
+
+$random = rand(1, 100);
+echo "$random\n";
+$gok = 0;
+
+while ($random != $gok) {
+
+    $gok = readline("gok een nummer tussen de 1 en 100 of type stop: ");
+
+    if ($gok === "stop") {
+        exit("uw heeft op stop gedrukt");
+    }
+
+    if ($random > $gok) {
+        echo "Ah wat jammer je moet wat hooger gokken.\n";
+    }
+
+    if ($random < $gok) {
+        echo " Oef je moet wat lager gokken.\n";
+    }
+
+    if ($random == $gok) {
+        echo "Gefeliciteerd je hebt goed gegokt.\n";
+    }
+    
+    if ($gok > 100){
+        exit ("gestopt omdat je boven de 100 ging");
+     }
+     
+     if ($gok < 1){
+        exit ("gestopt omdat je onder de 1 ging");
+     }
+}
