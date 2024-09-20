a15f3d3bbdf111ee663670a7aef76a9412fab4fb
GertNingbers <97081115@st.deltion.nl>
2024-09-20 12:11:07 +0200

    V4 quiz

--- test.php
+++ test.php
@@ -11,26 +11,27 @@ while ($random != $gok) {
     $gok = readline("gok een nummer tussen de 1 en 100 of type stop: ");
 
     if ($gok === "stop") {
-        exit("uw heeft op stop gedrukt");
+        exit("uw heeft op stop gedrukt.\n");
     }
+    
+    if ($gok > 100){
+        exit ("gestopt omdat je boven de 100 ging.\n");
+     }
+     
+     if ($gok < 1){
+        exit ("gestopt omdat je onder de 1 ging.\n");
+     }
 
-    if ($random > $gok) {
+     if ($random > $gok) {
         echo "Ah wat jammer je moet wat hooger gokken.\n";
     }
 
     if ($random < $gok) {
-        echo " Oef je moet wat lager gokken.\n";
+        echo "Oef je moet wat lager gokken.\n";
     }
-
+    
     if ($random == $gok) {
         echo "Gefeliciteerd je hebt goed gegokt.\n";
     }
     
-    if ($gok > 100){
-        exit ("gestopt omdat je boven de 100 ging");
-     }
-     
-     if ($gok < 1){
-        exit ("gestopt omdat je onder de 1 ging");
-     }
 }
