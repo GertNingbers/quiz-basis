9db9763402c1e127056c863da5c53d3561a7bab2
GertNingbers <97081115@st.deltion.nl>
2024-09-20 13:58:59 +0200

    V5 quiz

--- quiz.php
+++ quiz.php
@@ -1,21 +1,40 @@
 <?php
 
 echo "Welkom bij mijn quiz!\n";
-echo "\n";
 
-$maximum=1;
-$random = rand(1,100);
+$random = rand(1, 100);
 echo "$random\n";
-$invoer = readline ("gok een nummer tussen de 1 en 100: ");
-echo "\n";
-if ( $random == $invoer ) {
-    echo "Gefeliciteerd je hebt goed gegokt.\n"; 
-}
-if ( $random < $invoer ) {
-    echo " Oef je moet wat lager gokken.\n";
-}
-if ( $random > $invoer ) {
-    echo "Ah wat jammer je moet wat hooger gokken.\n";
-} 
+$gok = 0;
+$beurten=0;
+$getal1=1;
 
-?>
+while ($random != $gok) {
+
+    $gok = readline("gok een nummer tussen de 1 en 100 of type stop: ");
+    
+    if ($gok === "stop") {
+        exit("uw heeft op stop gedrukt.\n");
+    }
+    
+    if ($gok > 100){
+        exit ("gestopt omdat je boven de 100 ging.\n");
+    }
+     
+    if ($gok < 1){
+        exit ("gestopt omdat je onder de 1 ging.\n");
+    }
+
+    if ($random > $gok) {
+        echo "Ah wat jammer je moet wat hooger gokken.\n"; $beurten=$beurten + $getal1;
+    }
+
+    if ($random < $gok) {
+        echo "Oef je moet wat lager gokken.\n"; $beurten=$beurten + $getal1;
+    }
+    
+    if ($random == $gok) {
+        echo "Gefeliciteerd je hebt goed gegokt.\n"; $beurten=$beurten + $getal1;
+    }
+
+}
+echo "Je hebt het binnen $beurten gegokt";
